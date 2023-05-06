65c6ec1550592f5d5858f5d290ebab5fb585e508
Pavel Pinchuk <pavel.pinchuk@ascendle.com>
2023-05-06 13:22:52 +0200

    added metronome

---
+++ metronome/app.js
@@ -0,0 +1,26 @@
+const metronome = new Metronome();
+const tempo = document.getElementById('tempo');
+tempo.textContent = metronome.tempo;
+
+const playPauseIcon = document.getElementById('play-pause-icon');
+
+const playButton = document.getElementById('play-button');
+playButton.addEventListener('click', function() {
+    metronome.startStop();
+
+    if (metronome.isRunning) {
+        playPauseIcon.className = 'pause';
+    }
+    else {
+        playPauseIcon.className = 'play';
+    }
+
+});
+
+const tempoChangeButtons = document.getElementsByClassName('tempo-change');
+for (let i = 0; i < tempoChangeButtons.length; i++) {
+    tempoChangeButtons[i].addEventListener('click', function() {
+        metronome.tempo += parseInt(this.dataset.change);
+        tempo.textContent = metronome.tempo;
+    });
+}
